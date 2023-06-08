a487d2706d1a9d279ff4f26d6bbd6945430dc8cc
Ewerson Pulquerio <pulquerio.x@gmail.com>
2023-06-08 07:07:41 -0300

    Tentativa de adicionar o PostgreSql como banco, mas não deu certo

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'simplemooc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
