bcd32a9409e777f46598d0295ac9d5de9d76fa91
Ewerson Pulquerio <pulquerio.x@gmail.com>
2023-06-09 17:31:31 -0300

    Adicionado o arquivo de forms

--- simplemooc/app_courses/forms.py
+++ simplemooc/app_courses/forms.py
@@ -1,1 +1,7 @@
-irei colocar os forms aqui
+from django import forms
+
+class ContactCourse(forms.Form):
+    name = forms.CharField(label='Nome', max_length=100, required=False)
+    email = forms.EmailField(label='E-mail',)
+    message = forms.CharField(label='Mensagem/Dúvida', widget=forms.Textarea
+                          )
