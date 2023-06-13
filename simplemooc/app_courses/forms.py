77ec48083bdfce0bc5c4ddd8c25d8db52c4f4446
Ewerson Pulquerio <pulquerio.x@gmail.com>
2023-06-13 19:55:00 -0300

    Finalizado até aula 31 da seção 3 - Emails

--- simplemooc/app_courses/forms.py
+++ simplemooc/app_courses/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.core.mail import send_mail
+from django.conf import settings
+
+from app_core.mail import send_email_template
 
 class ContactCourse(forms.Form):
-    name = forms.CharField(label='Nome', max_length=100, required=False)
+    name = forms.CharField(label='Nome', max_length=100, required=True)
     email = forms.EmailField(label='E-mail',)
-    message = forms.CharField(label='Mensagem/Dúvida', widget=forms.Textarea
-                          )
+    message = forms.CharField(label='Mensagem/Dúvida', widget=forms.Textarea)
+
+    def send_mail(self, course):
+        subject = '[%s] Contato' % course
+        context = {
+            'name': self.cleaned_data['name'],
+            'email': self.cleaned_data['email'],
+            'message': self.cleaned_data['message']
+        }
+        template_name = 'courses/contact_email.html'
+        send_email_template(subject, template_name, context, [settings.CONTACT_EMAIL])
--- simplemooc/app_courses/views.py
+++ simplemooc/app_courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 # Create your views here.
 
 def courses(request):
@@ -20,8 +21,19 @@ def courses(request):
 
 def details (request, slug):
     course = get_object_or_404(Course, slug=slug)
-    context = {
-        'course': course
-    }
+    context = {}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST) #Se for método post deve seguir isso aqui
+        if form.is_valid():
+            context['is_valid'] = True
+            form.send_mail(course)
+            #print(form.cleaned_data)
+            #print(form.cleaned_data['name'])
+            form = ContactCourse()
+            
+    else:
+        form = ContactCourse()
+    context['form'] = form
+    context['course'] = course
     template_name = 'courses/details.html'
     return render(request, template_name, context)
