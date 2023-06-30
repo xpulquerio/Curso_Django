b7a9e33a55ddbf5b7a6ee21aacb202c445b6adf6
Ewerson Pulquerio <pulquerio.x@gmail.com>
2023-06-30 13:46:10 -0300

    Removida pasta .idea

--- simplemooc/app_accounts/models.py
+++ simplemooc/app_accounts/models.py
@@ -2,7 +2,7 @@ import re
 from django.db import models
 from django.core import validators
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, UserManager)
-
+from django.conf import settings
 class User (AbstractBaseUser, PermissionsMixin):
     username = models.CharField('Nome de usuário', max_length=30, unique=True,
                                 validators=[
@@ -35,4 +35,21 @@ class User (AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = 'Usuário'
         verbose_name_plural = 'Usuários'
+
+class PasswordReset(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name='Usuário',
+                                # related_name='resets'
+                             )
+    key = models.CharField('Chave', max_length=100, unique=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    confirmed = models.BooleanField('Confirmado?', default=False, blank=True)
+    def __str__(self):
+        return '{0} em {1}'.format(self.user, self.created_at)
+
+    class Meta:
+        verbose_name = 'Nova senha'
+        verbose_name_plural = 'Novas senhas'
+        ordering = ['-created_at']
+
+
     
